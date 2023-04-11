bc0a1eda8391fb2185379f5be4c3d7d98c4d6d0e
roshanmagar <roshanmgr130@gmail.com>
2023-04-11 08:02:55 +0545

    Array List

--- app/src/main/java/com/example/notebook/MainActivity.java
+++ app/src/main/java/com/example/notebook/MainActivity.java
@@ -9,18 +9,26 @@ import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     FloatingActionButton btnaddnote;
+    ArrayList<Note> notes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        notes = new ArrayList<>();
+        notes.add(new Note("sd","fgs","fds"));
+        notes.add(new Note("sd","fgs","fds"));
+        notes.add(new Note("sd","fgs","fds"));
+
         btnaddnote = findViewById(R.id.btnaddnote);
         RecyclerView rv= findViewById(R.id.recycle_notes);
-        rv.setAdapter(new NotesAdapter());
+        rv.setAdapter(new NotesAdapter(notes));
 
         btnaddnote.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/notebook/NotesAdapter.java
+++ app/src/main/java/com/example/notebook/NotesAdapter.java
@@ -3,11 +3,21 @@ package com.example.notebook;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewsHolder> {
+
+    ArrayList<Note> notes;
+
+    public NotesAdapter(ArrayList<Note> notes) {
+        this.notes = notes;
+    }
+
     @NonNull
     @Override
     public NotesViewsHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -17,18 +27,32 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewsHo
 
     @Override
     public void onBindViewHolder(@NonNull NotesViewsHolder holder, int position) {
+        holder.bindView(notes.get(position));
 
     }
 
     @Override
     public int getItemCount() {
-        return 10;
+        return notes.size();
     }
 
     class NotesViewsHolder extends RecyclerView.ViewHolder{
 
+        TextView category, title, description;
+
         public NotesViewsHolder(@NonNull View itemView) {
             super(itemView);
+            title = itemView.findViewById(R.id.display_note_title);
+            category = itemView.findViewById(R.id.display_note_category);
+            description = itemView.findViewById(R.id.display_note_description);
+
+        }
+
+        public void bindView(Note notes){
+            title.setText(notes.getTitle());
+            category.setText(notes.getCategory());
+            description.setText(notes.getDiscription());
+
         }
     }
 }
